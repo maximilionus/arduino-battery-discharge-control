af2d7e02f17c670d3ae2d388be614685ddf3b2be
maximilionus <maximilionuss@gmail.com>
2024-05-25 23:56:37 +0300

    Add support for OLED i2c display

--- arduino-battery-discharge-control.ino
+++ arduino-battery-discharge-control.ino
@@ -1,6 +1,12 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
 #define POWER_RELAY_PIN 2
 #define VOLTAGE_IN_PIN A0
 #define VOLTAGE_MIN_RAW 818
+
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 
 int getVoltageRaw() {
@@ -15,12 +21,21 @@ void switchRelay(bool enable) {
     }
 }
 
+
 void setup() {
     Serial.begin(9600);
     pinMode(VOLTAGE_IN_PIN, INPUT);
     pinMode(POWER_RELAY_PIN, OUTPUT);
 
     switchRelay(true);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+    display.display();
+    delay(5000);
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+
     Serial.println(F("Discharging started"));
 }
 
@@ -29,6 +44,12 @@ void loop() {
 
     Serial.print(current_voltage);
     Serial.println(" raw V");
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print(F("CURR: ")); display.println(current_voltage);
+    display.print(F("MIN: ")); display.println(VOLTAGE_MIN_RAW);
+    display.display();
 
     if (current_voltage <= VOLTAGE_MIN_RAW) {
         switchRelay(false);
