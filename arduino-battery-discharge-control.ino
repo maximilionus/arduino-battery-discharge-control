741d18bc4564ea7e382ca51053177c8a3b1d15f8
maximilionus <maximilionuss@gmail.com>
2024-05-25 17:54:52 +0300

    Initial commit

---
+++ arduino-battery-discharge-control.ino
@@ -0,0 +1,40 @@
+#define POWER_RELAY_PIN 2
+#define VOLTAGE_IN_PIN A0
+#define VOLTAGE_MIN_RAW 877
+
+
+int getVoltageRaw() {
+    return analogRead(VOLTAGE_IN_PIN);
+}
+
+void switchRelay(bool enable) {
+    if (enable) {
+        digitalWrite(POWER_RELAY_PIN, LOW);
+    } else {
+        digitalWrite(POWER_RELAY_PIN, HIGH);
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(VOLTAGE_IN_PIN, INPUT);
+    pinMode(POWER_RELAY_PIN, OUTPUT);
+
+    switchRelay(true);
+    Serial.println(F("Discharging started"));
+}
+
+void loop() {
+    int current_voltage = getVoltageRaw();
+
+    Serial.print(current_voltage);
+    Serial.println(" raw V");
+
+    if (current_voltage <= VOLTAGE_MIN_RAW) {
+        switchRelay(false);
+        Serial.println(F("Minimal charge reached, relay off"));
+        while (true) {}
+    }
+
+    delay(1000);
+}
